18b9092a3aafd40000214f81449614c923ff0ef6
Victor Abussafi <victorabussafi@gmail.com>
2025-02-12 13:38:43 +0000

    implement branding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import logo  from '../assets/images/kanban-logo.png';
+import logo  from '../assets/images/logo.png';
 
 const Navbar = () => (
   <nav className='navbar'>
